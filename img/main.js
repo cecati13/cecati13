c0145ede030d92b5db145c888f096214c06d35a2
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2021-11-12 19:29:33 -0600

    Clonado para iniciar el 12 de noviembre 2021

---
+++ img/main.js
@@ -0,0 +1,1 @@
+alert("Tenemos nueva oferta educativa");
---
+++ main.js
@@ -0,0 +1,2 @@
+alert("Tenemos lista la nueva oferta educativa para los meses de Noviembre y Diciembre del Ciclo Escolar 2021-2022.");
+alert("Contamos con las modalidades: a Distancia, Híbridos y Presenciales.");
