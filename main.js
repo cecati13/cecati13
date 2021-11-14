fe78986d41ecbe0e29876051094619457f807dba
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2021-11-13 23:23:18 -0600

    Menu Mobile, primeros estilos

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 // alert("Tenemos lista la nueva oferta educativa para los meses de Noviembre y Diciembre del Ciclo Escolar 2021-2022.");
 // alert("Contamos con las modalidades: a Distancia, Híbridos y Presenciales.");
+
+const menu = document.getElementById("menu");
+const nav = document.getElementById("navigator");
+
+menu.addEventListener("click", ()=> {
+    nav.classList.toggle("nav__hide");
+    nav.classList.toggle("nav__show");
+})
