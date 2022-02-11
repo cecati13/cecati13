078f237e4d645ca17d2bea92f71e0b9822ab2cb4
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2022-02-10 21:51:27 -0600

    Prueba Fetch API en Servidor Virtual, y algunos estilos iniciales

--- js/pruebaFetch.js
+++ js/pruebaFetch.js
@@ -1,15 +1,16 @@
 const azureURL = "https://cecati13api.azurewebsites.net";
 const cloudServerURL = "http://svo-5-191.servidoresvirtuales.mx";
+const localhost = "http://localhost:3500"
 const nodePrueba = document.getElementById("prueba");
 
 async function conexion(URL) {
     try {
-        const crud = await fetch(`${URL}/api/v1/products/`);        
+        const crud = await fetch(`${URL}/api/v1/educativeOffer`);        
         const response = await crud.json()
         console.log(response);        
         response.forEach(element => {
-            nodePrueba.innerHTML += `<div>Nombre: ${element.name} - Precio: ${element.price}
-             Departamento: ${element.departament} - URL Para imagen: ${element.image}</div>`
+            nodePrueba.innerHTML += `<div>Profesor: ${element.profesor} - especialidad: ${element.especialidad}
+             curso: ${element.curso} - Horas: ${element.horas}</div>`
         });
 
     } catch (error) {
