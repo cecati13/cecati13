3fabf6944969fffa4e77a80a9458b504d143ceb4
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2022-03-03 20:02:50 -0600

    Line con hr en vez de span y desde JS. HomePage para mobile 1era version terminada

--- js/homePage.js
+++ js/homePage.js
@@ -8,11 +8,11 @@ const arrayHomePage = [
     advertisements = {
         // ofertaActual : {
         //     title : "Información importante sobre el seguro escolar",
-        //     information : "El seguro escolar es una póliza de gastos médicos que garantiza la atención de forma gratuita en caso de sufrir un accidente escolar; debido a la pandemia y que la mayoría de nuestros cursos se imparten a distancia les estamos brindando información a los alumnos que deseen contratar este seguro",
-        //     note: "Nota. El Seguro Escolar contra Accidentes iniciará su operatividad en el momento que los alumnos asistan a sus escuelas de manera presencial y que haya contratado el mismo. El seguro Escolar tiene un costo de $65 que puede pagarse opcionalmente, cubre riesgos en las instalaciones del plantel y en el trayecto de ida y vuelta de las instalaciones educativas al domicilio (se aplican condiciones de la póliza). El costo es un solo pago de $65 y aplica para todos los cursos a los que se inscriban durante el ciclo escolar. Es recomendable para todos los cursos presenciales e híbridos. En caso de no contratar el seguro el plantel no se hace responsable por algún accidente que suceda dentro de las instalaciones",
-        //     button : "Conoce las condiciones del seguro escolar",
-        //     linkPage : "https://cecati13web.blob.core.windows.net/descargas/info_seguro_escolar.pdf",
-        //     animation: false
+        //     information : "El seguro escolar es una póliza de gastos médicos que garantiza la atención",
+        //     note : "",
+        //     button : "Cursos",
+        //     linkPage : "courses",
+        //     animation: false            
         // },    
         promocion : {
             title : "Promoción para cursos en marzo",
@@ -25,11 +25,17 @@ const arrayHomePage = [
             animation: true
         },
         // clases : {
-        //     title : "Promoción",
-        //     information : "Inscribete al primer curso por $300.00, e inscribete a $150.00",
-        //     button : "Inscríbete",
-        //     linkPage : "inscription",
-        //     animation: true
+        //     title : "Galeria de imagenes",
+        //     information : "Revisa alguans de nuestras imagenes disponibles. Conoce mejor tu plantel",
+        //     button : "Galeria",
+        //     note: "",
+        //     linkPage : "http://cecati13.com.mx/assets/galeria/portada1.jpeg",
+        //     animation: false
+        // },
+        // other : {
+        //     title : "Prueba extra",
+        //     information : "Una prueba mas con 4 elementos para ver como se ve sin boton ni elementos extras.",            
+        //     note: "",            
         // },
     }
 ];
@@ -44,31 +50,50 @@ class HomePage {
         return container;        
     }
     
-    countAnnounce(objArray){
-        
+    countAnnounce(objArray){        
         const containerAdvertisements = document.createElement("div");
         containerAdvertisements.className = "main__container__advertisements";
         const countAdvertisements = Object.keys(objArray).length;
+        let classAdvertisements = "main__container__advertisements--div"
+        if (countAdvertisements === 1) {
+            classAdvertisements = "main__container__advertisements--div main__container__advertisements--div--maxWidth"
+        }
         const names = Object.getOwnPropertyNames(objArray)
         let count = 0;
         while (count < countAdvertisements) {              
             const value = names[count];        
-            const container = this.announce(objArray[value]);
+            const container = this.announce(objArray[value], classAdvertisements);
             containerAdvertisements.appendChild(container);
             count++;
         }
         return containerAdvertisements;
     }
 
-    announce(obj){
+    announce(obj, stringClass){
         const container = document.createElement("div");
-        container.className = "main__container__advertisements--div";
-        container.innerHTML = `        
+        container.className = stringClass;
+        const absolutePath = /^http?:\/\//i;
+        let animate = "";        
+        if (obj.animation) {
+            animate = "buttonAnimate";
+        }
+        if (!absolutePath.test(obj.linkPage)) {
+            obj.linkPage = document.querySelector(`#${obj.linkPage}`);
+        }
+        if (obj.button === undefined) {
+            container.innerHTML = `
             <p class="main__container__advertisements--div__title">${obj.title}</p>
             <p>${obj.information}</p>
             <p class="main__container__advertisements--div__note">${obj.note}</p>
-            <a href="${obj.linkPage}" class="button__link buttonAnimate">${obj.button}</a>       
         `;
+        } else {
+            container.innerHTML = `
+            <p class="main__container__advertisements--div__title">${obj.title}</p>
+            <p>${obj.information}</p>
+            <p class="main__container__advertisements--div__note">${obj.note}</p>
+            <a href="${obj.linkPage}" class="button__link ${animate}">${obj.button}</a>
+            `;
+        }
         return container;
     }
 
@@ -79,7 +104,7 @@ class HomePage {
             <h3 class="main__container__welcome--title">${obj.title}</h3>
             <h3 class="main__container__welcome--age">${obj.year}</h3>
             `;
-            //<img src="${obj.image}" alt="Portada de CECATI 13">
+            
         return container
     }
 }
@@ -87,79 +112,3 @@ class HomePage {
 const nodeMain = document.querySelector("#main");
 const containerMain = new HomePage(arrayHomePage);
 nodeMain.appendChild(containerMain);
-
-
-
-
-// function createContainer () {
-//     const container = document.createElement("div");
-//     return container;
-// }
-
-// function welcome (obj) {
-//     const containerWelcome = createContainer();
-//     containerWelcome.className = "main__container__welcome";
-//     containerWelcome.innerHTML = `
-//     <h3 class="main__container__welcome--title">${obj.title}</h3>
-//     <h3 class="main__container__welcome--age">${obj.year}</h3>
-//     `;
-//     //<img class="main__container__welcome--img" src="${obj.image}">
-//     return containerWelcome;
-// }
-
-// function advertisementsTotal(obj) {
-//     const containerAdvertisements = createContainer();
-//     containerAdvertisements.className = "main__container__advertisements";
-//     const countAdvertisements = Object.keys(obj).length;
-//     let classAdvertisements = "main__container__advertisements--div"
-//     if (countAdvertisements === 1) {
-//         classAdvertisements = "main__container__advertisements--div main__container__advertisements--div--maxWidth"
-//     }
-//     const names = Object.getOwnPropertyNames(obj)
-//     let count = 0;
-//     while (count < countAdvertisements) {              
-//         const value = names[count];        
-//         const container = createAdvertisements(obj[value], classAdvertisements);
-//         containerAdvertisements.appendChild(container);
-//         count++;
-//     }
-//     return containerAdvertisements;
-// }
-
-// function createAdvertisements(obj, stringClass) {
-//     //crear el anuncio
-//     const container = createContainer();
-//     container.className = stringClass;
-
-//     container.innerHTML = `
-//         <p class="main__container__advertisements--div__title">${obj.title}</p>
-//         <p>${obj.information}</p>
-//         <p class="main__container__advertisements--div__note">${obj.note}</p>
-//     `;
-//     const button = document.createElement("a")
-//     button.textContent = obj.button;
-//     const absolutePath = /^https?:\/\//i;    
-//     if (absolutePath.test(obj.linkPage)) {
-//         button.href = obj.linkPage;
-//     } else {
-//         button.href = document.querySelector(`#${obj.linkPage}`);
-//     }
-//     button.className = "button__link";
-//     if (obj.animation) {
-//         button.classList.add("buttonAnimate");
-//     }
-//     if (obj.button) {        
-//         container.append(button);
-//     }     
-//     return container;
-// }
-
-// const containerWelcome = welcome(homepage[0]);
-
-// const containerMain = createContainer();
-// containerMain.className = "main__container";
-// containerMain.appendChild(containerWelcome);
-
-// const containerAdvertisements = advertisementsTotal(homepage[1]);
-// containerMain.appendChild(containerAdvertisements);
-// nodeMain.appendChild(containerMain);
