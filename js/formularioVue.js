27290ec327bb16f5a8a40d497af3c3a25e296b10
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2022-03-10 23:21:04 -0600

    Imagenes de portada prueba.html terminado, separacion de estilos de la base y el HomePage

--- js/formularioVue.js
+++ js/formularioVue.js
@@ -65,3 +65,4 @@ Vue.createApp({
           }
         }
     });
+
--- js/homePage.js
+++ js/homePage.js
@@ -2,7 +2,6 @@ const arrayHomePage = [
     cicloEscolar = {
         title : "Bienvenido",
         year : "2021-2022",
-        image: "http://cecati13.com.mx/assets/galeria/Portada5.jpeg"
     },
     
     advertisements = {
@@ -40,35 +39,80 @@ const arrayHomePage = [
     }
 ];
 
+const arrayImage = [
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes15.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes9.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes16.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes1.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes2.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes3.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes4.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes5.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes6.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes7.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes8.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes10.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes11.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes12.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes13.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes14.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes17.jpeg",
+    "https://cecati13web.blob.core.windows.net/galeria/zonas_comunes18.jpeg",
+]
+
 class HomePage {
     constructor(array) {
-        const container = document.createElement("div");
-        container.className = "main__container";
-        const containerWelcome = this.welcome(array[0]);
-        const containerAdvertisements = this.countAnnounce(array[1]);
-        container.append(containerWelcome, containerAdvertisements);
-        return container;        
-    }
-    
-    countAnnounce(objArray){        
-        const containerAdvertisements = document.createElement("div");
-        containerAdvertisements.className = "main__container__advertisements";
-        const countAdvertisements = Object.keys(objArray).length;
-        let classAdvertisements = "main__container__advertisements--div"
-        if (countAdvertisements === 1) {
-            classAdvertisements = "main__container__advertisements--div main__container__advertisements--div--maxWidth"
-        }
-        const names = Object.getOwnPropertyNames(objArray)
-        let count = 0;
-        while (count < countAdvertisements) {              
-            const value = names[count];        
-            const container = this.announce(objArray[value], classAdvertisements);
-            containerAdvertisements.appendChild(container);
-            count++;
-        }
-        return containerAdvertisements;
+        const nodeMain = document.querySelector("#main");
+        const containerWelcome = this.welcome(array[0]);        
+        const containerMain = this.createContainerMain(array[1]);
+        nodeMain.append(containerWelcome, containerMain);
+        this.removeClassAnimation();
     }
 
+    createContainerMain(objArray){
+        const containerMain = document.createElement("div");
+        containerMain.className = "main__container";
+        const containerImage1 = HomePage.imageCoverPage(0, "1");
+        containerMain.appendChild(containerImage1);
+
+        //revisar cuantos anuncios habra en el sitio
+        const countAdvertisements = Object.keys(objArray).length;
+        let classAdvertisements = "main__container__announce--div";       
+        const names = Object.getOwnPropertyNames(objArray)
+        let count = 0;
+        while (count < countAdvertisements) {
+            const container = document.createElement("div");
+            container.className = "main__container__announce";
+            const value = names[count];
+            const containerAnnounce = this.announce(objArray[value], classAdvertisements);            
+            container.appendChild(containerAnnounce);            
+            count++;
+            containerMain.appendChild(container);
+        }
+        
+        const positions = numberRandom()
+        const randomOne = positions[0];
+        const randomTwo = positions[1];        
+        const containerImage2 = HomePage.imageCoverPage(randomOne,"2");
+        const containerImage3 = HomePage.imageCoverPage(randomTwo,"3");
+
+        containerMain.append(containerImage2, containerImage3);
+        return containerMain; 
+    }
+    
+    static imageCoverPage(numberPosition,assignID){
+        const container = document.createElement("div");
+        container.className = "main__container__image image__change";
+        container.id=`containerImageCoverPage${assignID}`;            
+        container.innerHTML= `
+        <img src="${arrayImage[numberPosition]}"
+        alt="Imagen de portada del plantel"
+        id="imageCoverPage${assignID}"
+        >
+        `;        
+        return container;       
+    }    
+    
     announce(obj, stringClass){
         const container = document.createElement("div");
         container.className = stringClass;
@@ -82,15 +126,15 @@ class HomePage {
         }
         if (obj.button === undefined) {
             container.innerHTML = `
-            <p class="main__container__advertisements--div__title">${obj.title}</p>
+            <p class="main__container__announces--div__title">${obj.title}</p>
             <p>${obj.information}</p>
-            <p class="main__container__advertisements--div__note">${obj.note}</p>
+            <p class="main__container__announce--div__note">${obj.note}</p>
         `;
         } else {
             container.innerHTML = `
-            <p class="main__container__advertisements--div__title">${obj.title}</p>
+            <p class="main__container__announce--div__title">${obj.title}</p>
             <p>${obj.information}</p>
-            <p class="main__container__advertisements--div__note">${obj.note}</p>
+            <p class="main__container__announce--div__note">${obj.note}</p>
             <a href="${obj.linkPage}" class="button__link ${animate}">${obj.button}</a>
             `;
         }
@@ -99,16 +143,70 @@ class HomePage {
 
     welcome(obj){
         const container = document.createElement("div");
-        container.className = "main__container__welcome";
+        container.className = "main__welcome";
         container.innerHTML = `        
-            <h3 class="main__container__welcome--title">${obj.title}</h3>
-            <h3 class="main__container__welcome--age">${obj.year}</h3>
+            <h3 class="main__welcome--title">${obj.title}</h3>
+            <h3 class="mainr__welcome--age">${obj.year}</h3>
             `;
             
         return container
     }
+
+    removeClassAnimation(){
+        const node = document.getElementById(`containerImageCoverPage1`)    
+        node.classList.remove("image__change")
+    }    
 }
 
-const nodeMain = document.querySelector("#main");
 const containerMain = new HomePage(arrayHomePage);
-nodeMain.appendChild(containerMain);
+
+function numberRandom (){
+    const min = 0;
+    const max = 17;    
+
+    let n = 0;
+    let numero;
+    let uno = 0;
+    let dos = 0;    
+    do {
+        numero = Math.floor(Math.random() * (+max - +min)) + min;
+        if ((numero != uno) && (numero != dos)) {           
+            n++;
+            if (n == 1) {
+                uno = numero;
+            }
+            if (n == 2) {
+                dos = numero;
+            }
+        }
+    } 
+    while (n < 2);
+    const numbers = [uno, dos]    
+    return numbers;
+}
+
+function animationClass(number){
+    const node = document.getElementById(`containerImageCoverPage${number}`)
+    if (node.classList.contains("image__change")) {
+        node.classList.remove("image__change")        
+    }
+    node.classList.add("image__change");
+}
+
+function changeImage() {
+    setInterval( () => {
+        const positions = numberRandom()        
+        removeAddImage(2,positions[0])        
+        removeAddImage(3,positions[1])
+    }, 6000)
+//Ajustar tiempo segun animacion en la clase image__change en HomePage.css
+}
+
+function removeAddImage(ubication, numberArray){
+    const image = arrayImage[numberArray];
+    animationClass(ubication);
+    const nodeImage = document.getElementById(`imageCoverPage${ubication}`)
+    nodeImage.src = image;
+}
+
+window.onload = changeImage();
