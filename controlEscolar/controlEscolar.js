9b334d3dba9dfc886537c66300cebc0ca1bb0ee9
Damián Valenzuela <dvnegrete@gmail.com>
2023-01-10 12:23:45 -0600

    Preparacion para subir fichas de informacion de cursos desde el Front para consulta en /cursos. Conexion establecida con el Backend usando JWT

--- controlEscolar/controlEscolar.js
+++ controlEscolar/controlEscolar.js
@@ -1,8 +1,8 @@
 const app = Vue.createApp({
     data(){
         return {
-            API: "https://backend-cursos-cecati13.uc.r.appspot.com/API/v1",
-            //API:"http://localhost:3000/API/v1",
+            //API: "https://backend-cursos-cecati13.uc.r.appspot.com/API/v1/controlStudents",
+            API:"http://localhost:3000/API/v1/controlStudents",
             auth: false,
             fileSource: "",
             username: "",
@@ -19,7 +19,7 @@ const app = Vue.createApp({
                 password: password,
                 username: username
             };
-            const endpoint = `${this.API}/controlStudents/oauth`;
+            const endpoint = `${this.API}/oauth`;
             const response = await this.sendData(endpoint, obj);            
             if (response.token) {
                 localStorage.setItem("token", response.token);
@@ -48,7 +48,7 @@ const app = Vue.createApp({
                 contentType,
                 typeFile
             }
-            const endpoint = `${this.API}/controlStudents/getFile`;
+            const endpoint = `${this.API}/getFile`;
             const res = await this.sendData(endpoint, obj)
             if (res.error) {
                 this.message = res.error;
@@ -84,6 +84,20 @@ const app = Vue.createApp({
                 console.error(error);
               }
         },
+
+        async sendFiles(formFiles, API){
+            const response = await fetch( API, {
+              method: "post",
+              headers: {
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
+              },
+              //si vamos a subir archivos, se debe usar el formData, y no el json y quitar el headers
+              body: formFiles
+            })
+            const info = await response.json();
+            console.log("regresando de files: ", info)
+            return info
+          },
         
         createContentType(type) {            
             const obj = {
@@ -113,7 +127,16 @@ const app = Vue.createApp({
 
         clearMessage() {
             this.message = "";
-        }
+        },
+
+        async uploadFileFI(file){
+            const formFiles = new FormData;
+            formFiles.append("fileFI", file)
+            const enpoint = `${this.API}/fileInformation`
+            console.log("uploadFileFI", formFiles)
+            const res = await this.sendFiles(formFiles, enpoint)
+            console.log(res)
+        },
     },
 
     template: `
@@ -169,7 +192,62 @@ const app = Vue.createApp({
         <button>Enviar</button>
         <p class="message">{{ message }}</p>
     </form>
+
+    <v-uploadFile
+        v-if=auth
+        v-on:fileInformation="uploadFileFI"
+    >
     `      
+})
+
+app.component("v-findFileStudent", {
+    template: `
+    <form 
+        v-if=auth
+        v-on:submit="findFile"
+        class="formFile"
+    >
+        <label for="curp">CURP</label>
+        <input name="curp" v-on:focus="clearMessage">
+        <label for="typeDocument">Selecciona el Tipo de Archivo</label>
+        <select name="typeDocument" v-on:focus="clearMessage">
+            <option value="actaNacimiento">Acta de Nacimiento</option>
+            <option value="comprobanteDomicilio">Domicilio</option>
+            <option value="comprobanteEstudios">Grado de Estudios</option>
+        </select>
+        <label for="extension">Seleciona el tipo de archivo</label>
+        <select name="extension" v-on:focus="clearMessage">
+            <option value="jpg">jpg</option>
+            <option value="jpeg">jpeg</option>
+            <option value="pdf">pdf</option>
+            <option value="png">png</option>
+        </select>
+        <button>Enviar</button>
+        <p class="message">{{ message }}</p>
+    </form>
+    `
+})
+
+app.component("v-uploadFile", {
+    methods: {
+        uploadFI(e) {
+            e.preventDefault();            
+            const file = e.target.fileInformation.files[0];
+            this.$emit("fileInformation", file)
+        }
+    }, 
+
+    template: `
+    <form v-on:submit="uploadFI">
+        <label>Subir Ficha de información de Curso</label>
+        <input
+            type="file"
+            name="fileInformation"
+            accept=".pdf"
+        >
+        <button>Subir</button>
+    </form>
+    `
 })
 
 app.component("v-information", {
