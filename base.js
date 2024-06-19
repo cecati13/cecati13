658f436f7cc068cde191c0e630787d30953f93b0
Damian <dvnegrete@gmail.com>
2024-06-19 12:49:47 -0600

    fix(links a oferta educativa y calendario escolar): ocultar
    Ocultar temporalmente los accesos a Oferta Educativa y Calendario Escolar desde cualquier parte del sitio desde base.js

--- base.js
+++ base.js
@@ -1,78 +1,78 @@
 class Base {
-    createNAV(ub){
+    createNAV(ub) {
         const nav = document.createElement("nav");
-        nav.className ="nav__hide";
-        nav.id="navigator";
+        nav.className = "nav__hide";
+        nav.id = "navigator";
         nav.innerHTML = `
         <ul class="nav__show--ul">
-        <li>
-            <a id="home" href="${ub}/" class="nav__show--ul__link">Inicio</a>
-        </li>
-        <li>
-            <a id="specialties" href="${ub}/oferta" class="nav__show--ul__link">Oferta Educativa</a>
-        </li>
-        <li>
-            <a id="courses" href="${ub}/cursos" class="nav__show--ul__link">Cursos Disponibles</a>
-        </li>        
-        <li>
-            <a id="question" href="${ub}/preguntas" class="nav__show--ul__link">Preguntas Frecuentes</a>
-        </li>
-        <li>
-            <a id="services" href="${ub}/servicios" class="nav__show--ul__link">Servicios</a>
-        </li>
+            <li>
+                <a id="home" href="${ub}/" class="nav__show--ul__link">Inicio</a>
+            </li>
+            <li>
+                <a id="courses" href="${ub}/cursos" class="nav__show--ul__link">Cursos Disponibles</a>
+            </li>        
+            <li>
+                <a id="question" href="${ub}/preguntas" class="nav__show--ul__link">Preguntas Frecuentes</a>
+            </li>
+            <li>
+                <a id="services" href="${ub}/servicios" class="nav__show--ul__link">Servicios</a>
+            </li>
         </ul>   
         `;
-    return nav;
+        // <li>
+        //     <a id="specialties" href="${ub}/oferta" class="nav__show--ul__link">Oferta Educativa</a>
+        // </li>
+        return nav;
     }
-    createFOOTER(ub){
+    createFOOTER(ub) {
         const footerUL = document.createElement("ul");
         footerUL.className = "footer__contact"
         footerUL.innerHTML = `
         <li>
-        <a id="gallery" href="${ub}/galeria">Galería de Imágenes</a>
+            <a id="gallery" href="${ub}/galeria">Galería de Imágenes</a>
         </li>
         <li>
-        <a id="contact" href="${ub}/contacto">Contacto y Ubicación</a>
-        </li>
-        <li>
-        <a id="calendar" href="https://storage.googleapis.com/cecati13/Calendario%20Escolar%202022-2023.pdf" 
-        target="_blank">Calendario Escolar</a>        
+            <a id="contact" href="${ub}/contacto">Contacto y Ubicación</a>
         </li>
         <li>
             <a id="facebook" href="https://www.facebook.com/cecati.trece/about" target="_blank">
-                <div class="footer__contact--img--text">Facebook</div>
-                <img src="https://storage.googleapis.com/cecati13/assets/facebook-logo.png" 
-                alt="Facebook" class="footer__contact--img" height="50px">
+            <div class="footer__contact--img--text">Facebook</div>
+            <img src="https://storage.googleapis.com/cecati13/assets/facebook-logo.png" 
+            alt="Facebook" class="footer__contact--img" height="50px">
             </a>
             <a id="youtube" href="https://www.youtube.com/channel/UC5LX_ksGHFpydgsMJdWCv0A" target="_blank">
-                <div class="footer__contact--img--text">YouTube</div>
-                <img src="https://storage.googleapis.com/cecati13/assets/youtube-logo.png"
-                alt="YouTube" class="footer__contact--img" height="50px">
+            <div class="footer__contact--img--text">YouTube</div>
+            <img src="https://storage.googleapis.com/cecati13/assets/youtube-logo.png"
+            alt="YouTube" class="footer__contact--img" height="50px">
             </a>
         </li>
         `;
+        // <li>
+        //     <a id="calendar" href="https://storage.googleapis.com/cecati13/Calendario%20Escolar%202022-2023.pdf" 
+        // target="_blank">Calendario Escolar</a>
+        // </li>
         const footer = document.querySelector("#footer");
         const line = this.createLine();
-        footer.append(line,footerUL);
+        footer.append(line, footerUL);
     }
 
-    constructor() {        
-        const ubication = this.verifyHost();        
+    constructor() {
+        const ubication = this.verifyHost();
         this.createHeader(ubication);
         this.createFOOTER(ubication);
     }
-    
-    verifyHost(){
+
+    verifyHost() {
         let hostname = "";
         const verifying = /github/;
         const verifyGithub = verifying.test(window.location.hostname)
         if (verifyGithub) {
-            hostname = "/cecati13";            
+            hostname = "/cecati13";
         }
         return hostname;
     }
 
-    createHeader(ubication){
+    createHeader(ubication) {
         const menu = this.createMenu();
         const header = this.createHeaderPort(ubication);
         const nav = this.createNAV(ubication);
@@ -81,7 +81,7 @@ class Base {
         nodeHeader.append(menu, header, nav, line);
     }
 
-    createHeaderPort(ub){
+    createHeaderPort(ub) {
         const container = document.createElement("div");
         container.className = "header__port";
         container.innerHTML = `        
@@ -96,7 +96,7 @@ class Base {
         return container;
     }
 
-    createMenu(){
+    createMenu() {
         const containerSVG = document.createElement("div");
         containerSVG.className = "menu";
         containerSVG.innerHTML = `
@@ -109,12 +109,12 @@ class Base {
         `;
         return containerSVG;
     }
-    createLine(){
+    createLine() {
         const line = document.createElement("hr");
         line.className = "line";
         return line;
     }
-    
+
 }
 const base = new Base();
 
@@ -127,11 +127,11 @@ function menuAnimation(element) {
     nodeLineSVG.classList.toggle(element);
 }
 
-menu.addEventListener("click", ()=> {    
+menu.addEventListener("click", () => {
     if (nav.classList.contains("nav__hide")) {
         window.scroll(top);
-    }    
+    }
     menuSVG.map(menuAnimation);
     nav.classList.toggle("nav__hide");
-    nav.classList.toggle("nav__show");    
+    nav.classList.toggle("nav__show");
 })
