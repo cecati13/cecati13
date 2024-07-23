6ae1a5eeaf3a035db3b4956148a7bfe75b21b07c
dvnegrete <82685029+dvnegrete@users.noreply.github.com>
2024-07-23 03:26:40 +0000

    Deploying to gh-pages from @ cecati13/cecati13@20db8dc45f82919cc2a463bda76c784e140b3dea 🚀

--- galeria/galery.js
+++ galeria/galery.js
@@ -1,76 +1,38 @@
-const host = base.getFunctionsAPI();
-const URL = host + "/images"
+import { IMAGES } from "./../shared/images.js";
 const URL_BASE_IMAGE = "https://storage.googleapis.com/cecati13/galeria/";
 const container = document.querySelector(".carousel__list");
 
-function preloader() {
-    const nodePreloader = document.querySelector(".preloader")
-    nodePreloader.classList.toggle("preloader");
-}
-
 const functionGlider = () => {
-    new Glider(container, {
-        type: 'carousel',
-        dots: ".carousel__indicators",
-        arrows: {
-            prev: ".carousel__previous",
-            next: ".carousel__next"
-        }
-    });
-}
+  new Glider(container, {
+    type: "carousel",
+    dots: ".carousel__indicators",
+    arrows: {
+      prev: ".carousel__previous",
+      next: ".carousel__next",
+    },
+  });
+};
 
 function galleryContainer(array) {
-    const arrayContainer = []
-    array.forEach(element => {
-        const picture = document.createElement("div");
-        picture.innerHTML = `
+  const arrayContainer = [];
+  array.forEach((element) => {
+    const picture = document.createElement("div");
+    picture.innerHTML = `
         <picture class="container__picture">
-            <source srcset="${URL_BASE_IMAGE + element.imageSmall}" media="(max-width: 767px)">
-            <source srcset="${URL_BASE_IMAGE + element.imageMedium}" media="(max-width: 1023px)">
+            <source srcset="${
+              URL_BASE_IMAGE + element.imageSmall
+            }" media="(max-width: 767px)">
+            <source srcset="${
+              URL_BASE_IMAGE + element.imageMedium
+            }" media="(max-width: 1023px)">
             <img srcset="${URL_BASE_IMAGE + element.imageHigh}" media="100%"
             alt="${element.nombre}" class="gallery__img">        
         </picture>
         `;
-        arrayContainer.push(picture)
-    }
-    );
-    container.append(...arrayContainer)
-    preloader();
-    window.addEventListener("load", functionGlider())
+    arrayContainer.push(picture);
+  });
+  container.append(...arrayContainer);
+  window.addEventListener("load", functionGlider());
 }
 
-class ObjFromArray {
-    constructor(obj) {
-        const newArray = this.convert(obj)
-        return newArray;
-    }
-
-    convert(obj) {
-        const array = [];
-        for (const key in obj) {
-            const item = obj[key];
-            array.push(item);
-        }
-        return array;
-    }
-}
-
-async function conexion(URL) {
-    try {
-        const info = await fetch(`${URL}`);
-        const infoJSON = await info.json()
-        const response = new ObjFromArray(infoJSON);
-        galleryContainer(response);
-    } catch (error) {
-        console.log(error)
-        const nodeGallery = document.querySelector(".gallery");
-        const titleError = document.createElement("h3");
-        titleError.innerHTML = `
-        <h3 class="error__API">Lo sentimos, la información no esta disponible en este momento.
-        Por favor intenta más tarde, lamentamos los inconvenientes.</h3>`;
-        nodeGallery.appendChild(titleError);
-        preloader();
-    }
-}
-
-conexion(URL);
+galleryContainer(IMAGES);
--- homepage/homePage.js
+++ homepage/homePage.js
@@ -1,203 +1,177 @@
+import { IMAGES } from "./../shared/images.js";
 const API = base.getFunctionsAPI();
 const URL_BASE_IMAGE = "https://storage.googleapis.com/cecati13/galeria/";
 const containerMain = document.querySelector(".main__container");
 const widthPort = window.innerWidth;
 
-const arrayImage = [
-    "background_portada.jpg",
-]
+const arrayImage = ["background_portada.jpg"];
 
 class HomePage {
-    constructor(obj) {
-        this.createAnnounces(obj);
-        this.removeClassAnimation();
-        preloader();
-    }
+  constructor(obj) {
+    this.createAnnounces(obj);
+    this.removeClassAnimation();
+    preloader();
+  }
 
-    createAnnounces(object) {
-        const containerImage1 = HomePage.imageCoverPage(0, "1");
-        containerMain.appendChild(containerImage1);
+  createAnnounces(object) {
+    const containerImage1 = HomePage.imageCoverPage(0, "1");
+    containerMain.appendChild(containerImage1);
 
-        for (const key in object) {
-            if (Object.hasOwnProperty.call(object, key)) {
-                const element = object[key];
-                const container = document.createElement("div");
-                container.className = "main__container__announce";
-                const containerAnnounce = this.announce(element);
-                container.appendChild(containerAnnounce);
-                containerMain.appendChild(container);
-            }
-        }
-        const positions = numberRandom()
-        const randomOne = positions[0];
-        const randomTwo = positions[1];
-        const containerImage2 = HomePage.imageCoverPage(randomOne, "2");
-        const containerImage3 = HomePage.imageCoverPage(randomTwo, "3");
-
-        containerMain.append(containerImage2, containerImage3);
-    }
-
-    static imageCoverPage(numberPosition, assignID) {
+    for (const key in object) {
+      if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
         const container = document.createElement("div");
-        container.className = "main__container__image image__change";
-        container.id = `containerImageCoverPage${assignID}`;
-        container.innerHTML = `
+        container.className = "main__container__announce";
+        const containerAnnounce = this.announce(element);
+        container.appendChild(containerAnnounce);
+        containerMain.appendChild(container);
+      }
+    }
+    const positions = numberRandom();
+    const randomOne = positions[0];
+    const randomTwo = positions[1];
+    const containerImage2 = HomePage.imageCoverPage(randomOne, "2");
+    const containerImage3 = HomePage.imageCoverPage(randomTwo, "3");
+
+    containerMain.append(containerImage2, containerImage3);
+  }
+
+  static imageCoverPage(numberPosition, assignID) {
+    const container = document.createElement("div");
+    container.className = "main__container__image image__change";
+    container.id = `containerImageCoverPage${assignID}`;
+    container.innerHTML = `
         <img src="${URL_BASE_IMAGE}${arrayImage[numberPosition]}"
         alt="Imagen de portada del plantel"
         id="imageCoverPage${assignID}"
         >
         `;
-        return container;
-    }
+    return container;
+  }
 
-    announce(obj) {
-        const container = document.createElement("div");
-        container.className = "main__container__announce--div";
-        let animate = "";
-        const animation = obj.animacion.toUpperCase();
-        if (animation.includes("SI")) {
-            animate = "buttonAnimate";
-        }
-        if (obj.botones === undefined) {
-            container.innerHTML = `
+  announce(obj) {
+    const container = document.createElement("div");
+    container.className = "main__container__announce--div";
+    let animate = "";
+    const animation = obj.animacion.toUpperCase();
+    if (animation.includes("SI")) {
+      animate = "buttonAnimate";
+    }
+    if (obj.botones === undefined) {
+      container.innerHTML = `
             <p class="main__container__announces--div__title">${obj.titulo}</p>
             <p>${obj.informacion}</p>
             <p class="main__container__announce--div__note">${obj.notas}</p>
         `;
-        } else {
-            container.innerHTML = `
+    } else {
+      container.innerHTML = `
             <p class="main__container__announce--div__title">${obj.titulo}</p>
             <p>${obj.informacion}</p>
             <p class="main__container__announce--div__note">${obj.notas}</p>
             <a href="${obj.link}" class="button__link ${animate}">${obj.botones}</a>
             `;
-        }
-        return container;
     }
+    return container;
+  }
 
-    removeClassAnimation() {
-        const node = document.getElementById(`containerImageCoverPage1`)
-        node.classList.remove("image__change")
-    }
+  removeClassAnimation() {
+    const node = document.getElementById(`containerImageCoverPage1`);
+    node.classList.remove("image__change");
+  }
 }
 
 function createArrayImageFetch(imageJSON) {
-    for (const key in imageJSON) {
-        if (widthPort < 768) {
-            arrayImage.push(imageJSON[key].imageSmall);
-        }
-        else if (widthPort < 1024) {
-            arrayImage.push(imageJSON[key].imageMedium);
-        }
-        else {
-            arrayImage.push(imageJSON[key].imageHigh);
-        }
+  for (const key in imageJSON) {
+    if (widthPort < 768) {
+      arrayImage.push(imageJSON[key].imageSmall);
+    } else if (widthPort < 1024) {
+      arrayImage.push(imageJSON[key].imageMedium);
+    } else {
+      arrayImage.push(imageJSON[key].imageHigh);
     }
+  }
 }
 
 function numberRandom() {
-    const min = 0;
-    const max = arrayImage.length;
+  const min = 0;
+  const max = arrayImage.length;
 
-    let n = 0;
-    let numero;
-    let uno = 0;
-    let dos = 0;
-    do {
-        numero = Math.floor(Math.random() * (+max - +min)) + min;
-        if ((numero != uno) && (numero != dos)) {
-            n++;
-            if (n == 1) {
-                uno = numero;
-            }
-            if (n == 2) {
-                dos = numero;
-            }
-        }
+  let n = 0;
+  let numero;
+  let uno = 0;
+  let dos = 0;
+  do {
+    numero = Math.floor(Math.random() * (+max - +min)) + min;
+    if (numero != uno && numero != dos) {
+      n++;
+      if (n == 1) {
+        uno = numero;
+      }
+      if (n == 2) {
+        dos = numero;
+      }
     }
-    while (n < 2);
-    const numbers = [uno, dos]
-    return numbers;
+  } while (n < 2);
+  const numbers = [uno, dos];
+  return numbers;
 }
 
 function changeImage() {
-    opacityChange(2);
-    opacityChange(3);
-    setTimeout(() => {
-        const positions = numberRandom()
-        //const arrayPositions = indexArrayImage()        
-        changeOneImage(2, positions[0])
-        changeOneImage(3, positions[1])
-        //changeOneImage(2, position1)
-        //changeOneImage(3, position2)
-
-    }, 250);
-
-}
-
-//trabajar en el clousure para que las imagenes se pasen una a una y vaya aumentando
-const indexArrayImage = () => {
-    let positions = [1, 2];
-    //    let position2 = 2;
-    return {
-        function(increment) {
-            positions.forEach(element => {
-                element += increment;
-            });
-            return positions
-        }
-    }
-    return changeIndexArrayImage
+  opacityChange(2);
+  opacityChange(3);
+  setTimeout(() => {
+    const positions = numberRandom();
+    changeOneImage(2, positions[0]);
+    changeOneImage(3, positions[1]);
+  }, 300);
 }
 
 function opacityChange(ubication) {
-    const imagen = document.getElementById(`imageCoverPage${ubication}`)
-    imagen.style.opacity = "0.1";
+  const imagen = document.getElementById(`imageCoverPage${ubication}`);
+  imagen.style.opacity = "0.1";
 }
 
 function changeOneImage(ubication, numberArray) {
-    const imagen = document.getElementById(`imageCoverPage${ubication}`)
-    imagen.src = `${URL_BASE_IMAGE}` + arrayImage[numberArray];
-    imagen.style.opacity = "0.1";
-    setTimeout(() => {
-        imagen.style.opacity = "1"
-    }, 250)
+  const imagen = document.getElementById(`imageCoverPage${ubication}`);
+  imagen.src = `${URL_BASE_IMAGE}` + arrayImage[numberArray];
+  imagen.style.opacity = "0.1";
+  setTimeout(() => {
+    imagen.style.opacity = "1";
+  }, 250);
 }
 
 function slide() {
-    setTimeout(() => {
-        changeImage();
-        setInterval(changeImage, 7000);
-    }, 7000)
+  setTimeout(() => {
+    changeImage();
+    setInterval(changeImage, 7000);
+  }, 7000);
 }
 
 window.onload = slide();
 
 function preloader() {
-    const nodePreloader = document.querySelector(".preloader")
-    nodePreloader.classList.toggle("preloader");
+  const nodePreloader = document.querySelector(".preloader");
+  nodePreloader.classList.toggle("preloader");
 }
 
 async function conexion() {
-    try {
-        const infoImage = await fetch(API + "/images?size=17");
-        const imageJSON = await infoImage.json();
-        createArrayImageFetch(imageJSON);
+  try {
+    const imageJSON = IMAGES.slice(0, 17);
+    createArrayImageFetch(imageJSON);
 
-        const info = await fetch(API + "/homePage");
-        const infoJSON = await info.json();
-        const announce = new HomePage(infoJSON);
-
-    } catch (error) {
-        console.log(error)
-        preloader();
-        const titleError = document.createElement("h3");
-        titleError.innerHTML = `
+    const info = await fetch(API + "/homePage");
+    const infoJSON = await info.json();
+    const announce = new HomePage(infoJSON);
+  } catch (error) {
+    console.log(error);
+    preloader();
+    const titleError = document.createElement("h3");
+    titleError.innerHTML = `
         <h3 class="error__API">UPS!
         Lo sentimos, parece haber problemas con algunas funciones en el sitio.
         Por favor intenta más tarde, lamentamos los inconvenientes.</h3>`;
-        titleError.style.backgroundColor = "var(--mainColor)"
-        containerMain.appendChild(titleError);
-    }
+    titleError.style.backgroundColor = "var(--mainColor)";
+    containerMain.appendChild(titleError);
+  }
 }
 conexion();
